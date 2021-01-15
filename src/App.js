cbcd30e6d3a9ab8acff4f2dd1c51b1b8dffbdb38
= <=>
2021-01-14 23:59:46 -0300

    Create state compoments

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './App.css'
 
 import Comment from './components/Comment'
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My first project ReactJS</h1>
-      <Comment name="John" mail="john@gmail.com" >
-        Olá eu sou a mensagem.
-      </Comment>
-    </div>
-  );
+class App extends Component {
+  
+  state = {
+    comment: [
+      {
+        name: 'John',
+        mail: 'john@gmail.com',
+        message: 'Hello John'
+      },
+      {
+        name: 'Mary',
+        mail: 'Mary@gmail.com',
+        message: 'Hello Mary'
+      }
+    ]
+  }
+  
+  render(){
+    return (
+      <div className="App">
+        <h1>My first project ReactJS</h1>
+
+        {
+          this.state.comment.map((comment, indice )=> (
+            <Comment key={indice} name={comment.name} mail={comment.mail} >
+              {comment.message}
+            </Comment>
+
+          ) )
+        }
+        
+      </div>
+    );
+  }
 }
 
 export default App
